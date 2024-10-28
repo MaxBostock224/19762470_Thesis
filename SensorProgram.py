4981a8d5074faf760431c75f8bd0b85b88b98d39
MaxBostock224 <62872072+MaxBostock224@users.noreply.github.com>
2024-10-28 11:21:38 +0800

    Add files via upload

---
+++ SensorProgram.py
@@ -0,0 +1,85 @@
+import serial
+import time
+import csv
+import numpy as np
+from textwrap import wrap
+from pynput.keyboard import Key, Controller
+import onnxruntime as rt
+
+port = 'COM3'   # If incorrect COM port, replace with correct COM port.
+
+path = "Replace this with the path to the folder with the .onnx files"
+
+currentTime = 0
+previousTime = 0
+
+# Initialises a pynput controller for the keyboard that allows the software to press keys digitally.
+keyboard = Controller()
+
+try:
+    # Initialises connection to serial monitor
+    ser = serial.Serial(port, 9600, timeout=1)
+    
+    i = 10
+
+    while True:
+        # Read a line from the serial port
+        
+            line = ser.readline().decode('utf-8').rstrip()  # Reads in line from serial monitor, decodes it and strips it.
+            line = line.lstrip('a$')
+            splitLine = wrap(line, 5)   # Splits up the string every 5 characters, i.e. every channel
+            
+            # Occasionally, the sensor will fall out of sync and only supply two or three
+            # channels of data. To ensure this does not interfere with the process, the 
+            # program will only act if all four channels are present.
+            if(len(splitLine) == 4):
+                
+                # This loop exists to trim down the amount of data being processed.
+                if(i%10 == 0):
+                    # Print each channel for visual inspection
+                    print(splitLine[0]+"    "+splitLine[1]+"    "+splitLine[2]+"    "+splitLine[3])
+
+                    # Load ONNX model into session. Currently loading decision tree model.
+                    # If you want to load a different model type, replace 'dtree.onnx' with the appropriate
+                    # ONNX file name.
+                    # This code was based on the ONNX documentation:
+                    # https://onnx.ai/sklearn-onnx/index.html
+                    sess = rt.InferenceSession(path+"dtree.onnx", providers=["CPUExecutionProvider"])
+                    input_name = sess.get_inputs()[0].name
+                    label_name = sess.get_outputs()[0].name
+                    x = np.array(np.int64(splitLine[0])).reshape(1,1)
+                    
+                    # Runs the input channel through the ML model to classify it.
+                    # Prints the prediction for visual inspection.
+                    try:
+                        r = sess.run([label_name], {input_name: x})[0]
+                        print("Predicted label = "+str(r))
+                    except (RuntimeError) as e:
+                        print("Error with model prediction. Skipping line.")
+                    
+                    # Checks the current time.
+                    currentTime = time.time()
+                    
+                    # Only presses the key if 2 seconds have passed since the last activation.
+                    # This aims to prevent the key being pressed multiple times per muscle
+                    # activation.
+                    # Code based on response provided by user "Standard": https://stackoverflow.com/questions/58098177/execute-a-method-only-when-two-seconds-have-elapsed-from-last-execution-time
+                    if((currentTime-previousTime)>=2 and str(r)=="[1]"):
+                        Controller.press(Key.space)
+                        Controller.release(Key.space)
+                    previousTime = currentTime
+                    
+                    # Writes the value from the data channel being assessed and the assigned
+                    # label to a .csv file for later analysis.
+                    with open('C:/Users/maxbo/OneDrive/Desktop/FYP_Stuff/storedData.csv', 'a', newline = '') as file:
+                        write = csv.writer(file)
+                        write.writerow([splitLine[0],r])
+                        
+                i = i+1
+            else:
+                print("Buffering")
+except KeyboardInterrupt:
+    print('Exiting program.')
+finally:
+    if ser.is_open:
+        ser.close()
